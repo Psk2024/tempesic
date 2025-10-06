5718a5b9df3987b1a94fd17bad5101f87b05661e
Sateesh Kumar Pikkili <163013785+Psk2024@users.noreply.github.com>
2025-10-07 00:31:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -253,7 +253,7 @@ function showEmployeeModal(employeeId, rowIndex) {
             border-radius: 50%; 
             object-fit: cover; 
             border: 3px solid ${primaryColor};
-        " onerror="this.src='data:image/svg+xml;utf8,<svg xmlns=\\'http://www.w3.org/2000/svg\\' viewBox=\\'0 0 24 24\\' fill=\\'${primaryColor}\\' opacity=\\'0.7\\'><path d=\\'M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z\\' style=\\'fill: ${primaryColor};\\'/></svg>'; this.style.backgroundColor='${accentColor}'; this.style.border='none'; this.style.padding='20%';">`
+        " onerror="this.src='data:image/default.png;utf8,<svg xmlns=\\'http://www.w3.org/2000/svg\\' viewBox=\\'0 0 24 24\\' fill=\\'${primaryColor}\\' opacity=\\'0.7\\'><path d=\\'M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z\\' style=\\'fill: ${primaryColor};\\'/></svg>'; this.style.backgroundColor='${accentColor}'; this.style.border='none'; this.style.padding='20%';">`
         : 
         `<svg style="width: 80%; height: 80%; fill: ${primaryColor}; opacity: 0.7;" viewBox="0 0 24 24"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>`
       }
